5b13c0be893519da6e9f7f53b7e6e8d44607979d
Joyfull-Happiness <lajoie.bradley2@gmail.com>
2025-08-14 09:04:47 -0700

    updated toggle

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,41 +1,20 @@
 import Switch from "react-switch";
 import { useState } from "react";
+
 export default function Toggle() {
   const [checked, setChecked] = useState(false);
   function handleChange(checked) {
     setChecked(checked);
   }
+
   return (
     <>
-      <label>
+      <label className="toggle">
         <Switch onChange={handleChange} checked={checked} />
       </label>
       <p style={{ color: checked ? "blue" : "red" }}>
-        The switch is <span> {checked ? "on" : "off"} </span>
+        The switch is <span> {checked ? "active" : "inactive"} </span>
       </p>
     </>
   );
 }
-
-<div
-  style={{
-    width: "40px",
-    height: "20px",
-    backgroundColor: "#ff5c5c",
-    borderRadius: "20px",
-    position: "relative",
-    cursor: "pointer",
-  }}
->
-  <div
-    style={{
-      width: "18px",
-      height: "18px",
-      backgroundColor: "#fff",
-      borderRadius: "50%",
-      position: "absolute",
-      top: "1px",
-      right: "1px",
-    }}
-  ></div>
-</div>;
